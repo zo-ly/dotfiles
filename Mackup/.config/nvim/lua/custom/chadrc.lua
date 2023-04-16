438c944a8f72b8c2c7bb0ee5bfc61c90b507fa7c
zo.ly <zo.ly@outlook.com>
2023-04-16 23:55:21 +0800

    update nvchad

--- Mackup/.config/nvim/lua/custom/chadrc.lua
+++ Mackup/.config/nvim/lua/custom/chadrc.lua
@@ -17,7 +17,7 @@ M.ui = {
   hl_override = highlights.override,
   hl_add = highlights.add,
 
-    statusline = {
+  statusline = {
     theme = "minimal", -- default/vscode/vscode_colored/minimal
     -- default/round/block/arrow separators work only for default statusline theme
     -- round and block will work for minimal theme only
@@ -26,6 +26,10 @@ M.ui = {
 
   nvdash = {
     load_on_startup = true,
+  },
+
+  cmp = {
+    icons = false
   }
 }
 
