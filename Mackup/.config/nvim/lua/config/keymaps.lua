c8557e0a2178816049dfbc7733f35f3c02caa3b0
zo.ly <zo.ly@outlook.com>
2026-03-06 14:13:30 +0800

    ✨ feat(keymaps): add clipboard support for yank operation in normal and visual modes

--- Mackup/.config/nvim/lua/config/keymaps.lua
+++ Mackup/.config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+vim.keymap.set({ "n", "v" }, "Y", '"*y')
 vim.keymap.set("n", "H", "H")
 vim.keymap.set("n", "L", "L")
 vim.keymap.set("n", "J", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer" })
